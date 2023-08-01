15bb428b36a6f1bcc7c2cbc3f97c22981a3ff854
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2023-08-01 11:18:41 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,35 @@
+Config = {}
+
+Config.VehBlacklist = true
+Config.Vehs = {
+    "vigilante",
+    "hydra",
+    "buzzard",
+    "deluxo",
+    "avenger",
+    "akula",
+    "apc",
+    "barrage",
+    "caracara",
+    "cargobob",
+    "chernobog",
+    "hunter",
+    "insurgent",
+    "starling",
+    "lazer",
+    "bombushka",
+    "savage",
+    "rhino",
+    "khanjali"
+}
+
+Config.PropsBlacklist = true
+Config.Props = {
+    "prop_cs_burger_01"
+}
+
+Config.PedsBlacklist = true
+Config.Peds = {
+}
+
+Config.DisablePickups = true
